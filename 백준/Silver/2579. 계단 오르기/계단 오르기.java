c7f56aba59df12de7c3652d0803715f989b8cee8
zzu-yaaa <110540359+zzu-yaaa@users.noreply.github.com>
2024-04-06 02:35:24 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,25 +1,37 @@
 import java.io.*;
 
 public class Main{
+    static int[] arr = new int[301];
+
+    static int[] result = new int[301];
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int num = Integer.parseInt(br.readLine());
-        int[] arr = new int[301];
         for(int i = 1; i<=num;i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] result = new int[301];
         result[1] = arr[1];
         result[2] = arr[1] + arr[2];
         result[3] = Math.max(arr[1],arr[2]) + arr[3];
-        for(int i=4;i<=num;i++){
-            result[i] = Math.max(result[i-3]+arr[i-1], result[i-2]) + arr[i];
-        }
-
+        func(num);
+        
         bw.write(String.valueOf(result[num]));
         bw.flush();
+    }
+
+    private static int func(int n){
+        if(n == 1 || n==2 || n==3){
+            return result[n];
+        }
+
+        if(result[n] != 0){
+            return result[n];
+        }
+
+        return result[n] = Math.max(func(n-3)+arr[n-1],func(n-2))+arr[n];
     }
 }
