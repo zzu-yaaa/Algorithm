71604e1c6dad8ba2372f5cfb074428736f4d6279
zzu-yaaa <110540359+zzu-yaaa@users.noreply.github.com>
2024-05-21 16:21:08 +0900

    [Gold IV] Title: 여행 가자, Time: 200 ms, Memory: 18216 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static int[] travel;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        arr = new int[n+1];
+        for(int i=1;i<=n;i++){
+            arr[i] = i;
+        }
+
+        String[] inputArr;
+        for(int i=1;i<=n;i++){
+           inputArr = br.readLine().split(" ");
+           for(int j=1;j<=n;j++){
+               int x = Integer.parseInt(inputArr[j-1]);
+               if (x == 0) {
+                   continue;
+               }
+               union(i,j);
+           }
+        }
+
+        inputArr = br.readLine().split(" ");
+        travel = new int[m];
+        for (int i = 0; i < m; i++) {
+            travel[i] = Integer.parseInt(inputArr[i]);
+        }
+
+        boolean possible = true;
+        int root = find(travel[0]);
+        for (int i = 1; i < m; i++) {
+            if (find(travel[i]) != root) {
+                possible = false;
+                break;
+            }
+        }
+
+        if(possible){
+            bw.write("YES");
+        }
+        else{
+            bw.write("NO");
+        }
+
+//        for (int i : arr) {
+//            bw.write(String.valueOf(i)+" ");
+//        }
+        bw.flush();
+    }
+
+    private static void union(int a, int b){
+        int x = find(a);
+        int y = find(b);
+
+        arr[y] = x;
+    }
+
+    private static int find(int a){
+        if(arr[a] == a){
+            return a;
+        }
+        return arr[a] = find(arr[a]);
+    }
+}
