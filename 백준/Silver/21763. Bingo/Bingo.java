d2833fca5f80f41bc82dd52da36a490b0eedb669
zzu-yaaa <110540359+zzu-yaaa@users.noreply.github.com>
2024-08-06 23:20:56 +0900

    [Silver II] Title: Bingo, Time: 132 ms, Memory: 14412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21763. Bingo/Bingo.java
+++ 백준/Silver/21763. Bingo/Bingo.java
@@ -14,46 +14,70 @@ public class Main {
             bw.write("YES\n");
             bw.write(".");
         }
-        else{
-            if (((n - 1) * n) - 1 < k) {
-                bw.write("NO\n");
-            } else {
-                bw.write("YES\n");
-                int[][] out = new int[n][n];
-                for (int[] temp : out) {
-                    Arrays.fill(temp, 0);
-                }
+        else if(n == 2 && k >= 2){
+            bw.write("NO\n");
+        }
+        else if ( (n - 1) * n  < k ) {
+            bw.write("NO\n");
+        }
+        else {
+            bw.write("YES\n");
 
-                int cnt = 0;
-                int full = 0;
-                for (int i = 0; i < n; i++) {
-                    for (int j = 0; j < n - 1; j++) {
-                        if(i==n-1 && j==0) continue;
-                        if (cnt >= k) {
-                            full = 1;
-                            break;
-                        }
-                        out[i][(i + j) % n] = 1;
+            //배열을 .으로 채운다 (. = 0)
+            int[][] out = new int[n][n];
+            for (int[] temp : out) {
+                Arrays.fill(temp, 0);
+            }
+
+            //#의 개수
+            int cnt = 0;
+
+            //대각선 빙고가 아닌 곳 부터 #으로
+            for(int i=0; i<n && cnt < k; i++){
+                for(int j=0; j<n && cnt < k; j++){
+                    if(i!=j && i+j != n-1){
+                        out[i][j] = 1;
                         cnt += 1;
                     }
-                    if(full == 1){
-                        break;
-                    }
-                }
-
-                for(int i = 0; i < n; i++){
-                    for(int j = 0; j < n; j++){
-                        if(out[i][j] == 1){
-                            bw.write("#");
-                        }
-                        else{
-                            bw.write(".");
-                        }
-                    }
-                    bw.write("\n");
                 }
             }
+
+            //오른쪽 대각선(\)의 양 끝점을 #으로 바꾼다
+            if(cnt < k){
+                out[0][0] = 1;
+                cnt +=1;
+            }
+
+            if(cnt < k){
+                out[n-1][n-1] = 1;
+                cnt += 1;
+            }
+
+            //왼쪽 대각선(/)의 양 끝점 및 중심점을 제외하고 #으로 바꾼다
+            for(int i=1; i<n-1 && cnt<k; i++){
+                for(int j=1; j<n-i && cnt<k ; j++){
+                    if(i+j == n-1 && i != j){
+                        out[i][j] = 1;
+                        cnt += 1;
+                    }
+                }
+            }
+
+
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(out[i][j] == 1){
+                        bw.write("#");
+                    }
+                    else{
+                        bw.write(".");
+                    }
+                }
+                bw.write("\n");
+            }
         }
+
+        bw.flush();
 
         br.close();
         bw.close();
