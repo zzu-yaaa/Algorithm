c93f1669b1d97f889cdc9934964047de97ad4a9a
zzu-yaaa <110540359+zzu-yaaa@users.noreply.github.com>
2025-01-13 17:14:22 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,34 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-        public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        ArrayList<Integer> arr = new ArrayList<>();
-        int height=0;
-        for(int i = 0;i<9;i++){
-            arr.add(Integer.parseInt(br.readLine()));
-            height += arr.get(i);
+
+        int[] height = new int[9];
+        int total = 0;
+
+        for(int i=0; i<9 ; i++){
+            height[i] = Integer.parseInt(br.readLine());
+            total += height[i];
         }
+
+        Arrays.sort(height);
 
         int flag = 0;
-        for(int i = 0; i<9;i++){
-            for(int j=i+1;j<9;j++){
-                if(height - arr.get(i) - arr.get(j) == 100){
-                    arr.remove(i);
-                    arr.remove(j-1);
+        for(int i=8 ; i>=0 ; i--){
+            for(int j=i-1 ; j>=0 ; j--){
+                if(flag == 0 && height[i]+height[j] == total - 100){
+                    height[i] = 0;
+                    height[j] = 0;
                     flag = 1;
                 }
-                if (flag == 1) break;
             }
-            if (flag == 1) break;
         }
 
-        Collections.sort(arr);
-        for (Integer i : arr) {
-            bw.write(i + "\n");
+        for(int i=0; i<9 ; i++){
+            if(height[i] != 0){
+                bw.write(String.valueOf(height[i])+"\n");
+            }
         }
+
         bw.flush();
+
     }
 }
