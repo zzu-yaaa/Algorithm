24a9ee3cfbfa149c69a8edc8d0eed94a09e0392b
zzu-yaaa <110540359+zzu-yaaa@users.noreply.github.com>
2025-04-05 11:07:49 +0900

    [level 2] Title: 모음 사전, Time: 5.18 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int answer = 0;
+    public int cnt = 0;
+    public String[] alphabet = {"A", "E", "I", "O", "U"};
+    
+    public int solution(String word) {
+        func("", word);
+        return answer;
+    }
+    
+    public void func(String str, String word){
+        if(str.equals(word)){
+            answer = cnt;
+            return;
+        }
+        if(str.length() == 5){
+            return;
+        }
+        
+        for(int i=0; i<5; i++){
+            cnt += 1;
+            func(str+alphabet[i], word);
+        }
+    }
+}
