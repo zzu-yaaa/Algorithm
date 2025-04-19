dcc58764f511a3e9a3ede7d2a2d5b9648a5767c3
zzu-yaaa <110540359+zzu-yaaa@users.noreply.github.com>
2025-04-19 19:05:41 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 928 ms, Memory: 122324 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int cnt = 1;
+    public static ArrayList<ArrayList<Integer>> graph;
+    public static int[] vis;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] in = br.readLine().split(" ");
+        int n = Integer.parseInt(in[0]);
+        int m = Integer.parseInt(in[1]);
+        int r = Integer.parseInt(in[2]);
+
+        graph = new ArrayList<>();
+        for(int i=0; i<n+1; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        int[] vis = new int[n+1];
+
+        for(int i=0; i<m; i++){
+            in = br.readLine().split(" ");
+            int a = Integer.parseInt(in[0]);
+            int b = Integer.parseInt(in[1]);
+
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        for(int i=0; i<n+1; i++){
+            Collections.sort(graph.get(i), Collections.reverseOrder());
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(r);
+        vis[r] = 1;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+
+            for(int i=0; i<graph.get(node).size(); i++){
+                int cur = graph.get(node).get(i);
+                if(vis[cur] == 0){
+                    cnt += 1;
+                    vis[cur] = cnt;
+                    q.add(cur);
+                }
+            }
+        }
+
+        for(int i=1; i<n+1; i++){
+            bw.write(vis[i] + "\n");
+        }
+
+        bw.flush();
+    }
+}
