4f245815b5541c7aec7cee01a28b2909ec8620f4
zzu-yaaa <jy122032@gmail.com>
2025-05-05 14:53:18 +0900

    [Easy] 88. Merge Sorted Array / 27. Remove Element

---
+++ 리트코드/Easy/88. Merge Sorted Array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<m; i++){
+            list.add(nums1[i]);
+        }
+        for(int i=0; i<n; i++){
+            list.add(nums2[i]);
+        }
+
+        Collections.sort(list);
+
+        for(int i=0; i<m+n;i++){
+            nums1[i] = list.get(i);
+        }
+    }
+}
